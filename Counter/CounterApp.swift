d5e9e83deae1ae85e655a5a3fbd71cdd228ba2ad
Ali Syed <alisyed0206@gmail.com>
2025-12-01 12:56:48 -0500

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Ali Syed on 2025-12-01.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
